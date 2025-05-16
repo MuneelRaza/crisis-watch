8236f5f5e7c98904b801712d39f405ae2b5bc20a
Muneel Raza <89910039+MuneelRaza@users.noreply.github.com>
2025-05-16 22:22:21 +0500

    Update import_llm.py

--- utils/import_llm.py
+++ utils/import_llm.py
@@ -1,7 +1,9 @@
+from langchain_together import ChatTogether
+from langchain_nvidia_ai_endpoints import ChatNVIDIA
 from dotenv import load_dotenv
+
 load_dotenv()
 
-from langchain_nvidia_ai_endpoints import ChatNVIDIA
 
 
 
@@ -9,3 +11,8 @@ llm = ChatNVIDIA(model="mistralai/mixtral-8x7b-instruct-v0.1")
 
 # result = llm.invoke("Write a ballad about LangChain.")
 # print(result.content)
+
+llama = 'meta/llama-3.3-70b-instruct'
+
+llama_model = ChatNVIDIA(model=llama)
+
